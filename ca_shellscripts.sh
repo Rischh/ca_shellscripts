b2cbd48cd34ccb17207c8759fcdc038a27c83272
samuel-risch <rischebourg.samuel@gmail.com>
2023-10-30 15:24:56 +0100

    FIN

--- ca_shellscripts.sh
+++ ca_shellscripts.sh
@@ -26,3 +26,133 @@ if [ "$BIRTHDAY" == "Saturday" ] ; then
 else
     echo "BIRTHDAY is incorrect - please retry"
 fi
+
+
+
+#!/bin/bash
+
+NAMES=( John Eric Jessica )
+# write your code here
+NUMBERS=( 1 2 3 )
+STRINGS=( "hello" "world" )
+NumberOfNames=${#NAMES[@]}
+second_name=${NAMES[1]}
+echo ${NUMBERS[@]}
+echo ${STRINGS[@]}
+echo "The number of names listed in the NAMES array: $NumberOfNames"
+echo "The second name on the NAMES list is:" ${second_name}
+
+#!/bin/bash
+
+COST_PINEAPPLE=50
+
+COST_BANANA=4
+
+COST_WATERMELON=23
+
+COST_BASKET=1
+
+TOTAL=$(($COST_PINEAPPLE + $COST_BANANA + $COST_WATERMELON + $COST_BASKET))
+
+echo "Total Cost is $TOTAL"
+
+#!/bin/bash
+
+BUFFETT="Life is like a snowball. The important thing is finding wet snow and a really long hill."
+
+    # write your code here
+    ISAY=$BUFFETT
+    change1=${ISAY[@]/snow/foot}
+    change2=${change1[@]//snow/}
+    change3=${change2[@]/finding/getting}
+    loc=`expr index "$change3" 'w'`
+    ISAY=${change3::$loc+2}
+
+# Test code - do not modify
+echo "Warren Buffett said:"
+echo $BUFFETT
+echo "and I say:"
+echo "$ISAY"
+
+#!/bin/bash
+# change these variables
+NUMBER=16
+APPLES=16
+KING="LUIS"
+# modify above variables
+# to make all decisions below TRUE
+if [ $NUMBER -gt 15 ] ; then
+  echo 1
+fi
+if [ $NUMBER -eq $APPLES ] ; then
+  echo 2
+fi
+if [[ ($APPLES -eq 12) || ("$KING" = "LUIS") ]] ; then
+  echo 3
+fi
+if [[ $(($NUMBER + $APPLES)) -le 32 ]] ; then
+  echo 4
+fi
+
+#!/bin/bash
+NUMBERS=(951 402 984 651 360 69 408 319 601 485 980 507 725 547 544 615 83 165 141 501 263 617 865 575 219 390 237 412 566 826 248 866 950 626 949 687 217 815 67 104 58 512 24 892 894 767 553 81 379 843 831 445 742 717 958 609 842 451 688 753 854 685 93 857 440 380 126 721 328 753 470 743 527)
+
+# write your code here
+for gg in ${NUMBERS[@]}; do
+	
+    if [ $gg == 237 ]; then
+    	break;
+    elif [ $(($gg % 2)) == 0 ]; then
+    	echo $gg
+    fi
+done
+
+	#!/bin/bash
+	# enter your array comparison code here
+	# initialize arrays a b c
+	a=(3 5 8 10 6) 
+	b=(6 5 4 12) 
+	c=(14 7 5 7)
+	#comparison of first two arrays a and b
+	for x in "${a[@]}"; do 
+	  in=false 
+	  for y in "${b[@]}"; do 
+	    if [ $x = $y ];then 
+	      # assigning the matching results to new array z
+	      z[${#z[@]}]=$x
+	    fi
+	  done 
+	done
+	#comparison of third array c with new array z
+	for i in "${c[@]}"; do 
+	  in=false
+	  for k in "${z[@]}"; do
+	    if [ $i = $k ];then
+	      # assigning the matching results to new array j
+	      j[${#j[@]}]=$i
+	    fi
+	  done 
+	done 
+	# print content of array j
+	echo ${j[@]}
+
+	#!/bin/bash
+# enter your function code here
+
+function ENGLISH_CALC {
+  a=$1
+  b=$3
+  signn=$2
+  if [ $signn == "plus" ]; then
+    echo "$a + $b = $(($a+$b))"
+  elif [ $signn == "minus" ]; then
+    echo "$a - $b = $(($a-$b))"
+  elif [ $signn == "times" ]; then
+    echo "$a * $b = $(($a*$b))"
+  fi
+}
+
+# testing code
+ENGLISH_CALC 3 plus 5
+ENGLISH_CALC 5 minus 1
+ENGLISH_CALC 4 times 6
